9e2692028a020a64195b40d8f7a2d1b3c5a57806
bhuvan771 <bhuvankumar13995@gmail.com>
2025-04-09 14:59:52 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
         {/*footer*/}
         <footer className="bg-blue-50 py-12">
         <div className="container mx-auto px-4 text-center text-gray-600"> 
-          <p>Made by YRB Coder's</p>
+          <p>Made by YRB Coders</p>
         </div>
         </footer>
       </body>
